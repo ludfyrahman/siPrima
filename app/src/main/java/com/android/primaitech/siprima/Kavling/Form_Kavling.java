df49427ccb4306281d25de45034a7af457192b6a
ludfyrahman <ludfyr@gmail.com>
2019-08-25 22:57:32 +0700

    design form kavling dan karyawan

---
+++ app/src/main/java/com/android/primaitech/siprima/Kavling/Form_Kavling.java
@@ -0,0 +1,15 @@
+package com.android.primaitech.siprima.Kavling;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.android.primaitech.siprima.R;
+
+public class Form_Kavling extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_form_kavling);
+    }
+}
